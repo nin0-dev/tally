b0004837f837d8d01732d579916df8c8d8cb5416
nin0 <personal@nin0.dev>
2026-05-17 07:53:33 -0400

    backend: deck deletion

--- backend/src/routes/decks.ts
+++ backend/src/routes/decks.ts
@@ -23,4 +23,26 @@ export function setupDeckRoutes() {
 
 		return { id };
 	});
+
+	server.delete("/deck/:id", async (req, res) => {
+		const { id }: { id: string } = req.params as any;
+		if (!id || !/^[0-9a-f]{12}$/.test(id))
+			return void res.status(400).send();
+		const u = await getUserIDForRequest(req);
+		if (!u) return void res.status(401).send();
+
+		try {
+			var deck = await db
+				.selectFrom("decks")
+				.select("owner")
+				.where("id", "=", id)
+				.executeTakeFirstOrThrow();
+		} catch {
+			return void res.status(404).send();
+		}
+		if (deck.owner !== u) return void res.status(404).send();
+
+		await db.deleteFrom("decks").where("id", "=", id).execute();
+		res.status(204).send();
+	});
 }
