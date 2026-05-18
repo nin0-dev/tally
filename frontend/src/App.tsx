ec1e98af2344b3b9d55f7e753b06cf0b88a83345
nin0 <personal@nin0.dev>
2026-05-17 20:21:20 -0400

    frontend: init

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Switch, Route } from "wouter";
+import Home from "./pages/Home";
+
+export default function App() {
+	return (
+		<>
+			<Switch>
+				<Route path="/" component={Home} />
+			</Switch>
+		</>
+	);
+}
