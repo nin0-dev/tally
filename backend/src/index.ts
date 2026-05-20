3ec52117058e3fa998ca848c36181566b972d7d1
nin0 <personal@nin0.dev>
2026-05-20 05:45:47 -0400

    backend: add CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,9 +3,16 @@ import { Yapper } from "./utils/Yapper.js";
 import { setupAccountRoutes } from "./routes/accounts.js";
 import { setupDeckRoutes } from "./routes/decks.js";
 import { setupPlayRoutes } from "./routes/plays.js";
+import cors from "@fastify/cors";
 
 export const server = Fastify({
 	loggerInstance: new Yapper()
+});
+
+server.register(cors, {
+	origin: [(process.env.ALLOWED_CLIENT_ORIGINS ?? "").trim().split(",")],
+	methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+	credentials: true
 });
 
 setupAccountRoutes();
